5e3842ecf090c237d2117ba20af60a33f8a00bc0
Sowjanya Bulusu <48425332+sowjibulusu@users.noreply.github.com>
2019-10-03 11:22:06 +0200

    Add files via upload

---
+++ Decision_tree.py
@@ -0,0 +1,45 @@
+#Decision Tree Regression
+
+#Regression Template
+
+#Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pa
+
+#Importing the Dataset
+dataset = pa.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values #X is Matrix and Y is a vector
+y = dataset.iloc[:, 2].values
+
+'''#Splitting the dataset into Training Set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+''''''For Training Set we need to fit and Transform where as for test set we
+only need to transform, because it is already fitted to training set''''''
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+'''
+#Fitting Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(x, y)
+
+#predicting a new result with regression
+Required_Salary_of_new_employee = [[6.5],[0]]
+y_pred = regressor.predict(Required_Salary_of_new_employee)
+
+#Visualisng Decision Tree regression results (For Higher Resolution and Smoother Curve)
+x_grid = np.arange(min(x), max(x), 0.1)
+x_grid = x_grid.reshape(len(x_grid), 1)
+plt.scatter(x, y, color = 'red')
+plt.plot(x_grid, regressor.predict(x_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
